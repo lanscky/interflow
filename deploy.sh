1064a23ec83e02096574a3ae84600649dd42ec66
Lanscky <lansckytshinkola9@gmail.com>
2025-08-28 18:06:00 +0100

    add : deploy

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "Déploiement en cours..."
+
+cd /var/www/interflow
+
+# Pull du dernier code
+git reset --hard
+git pull origin main
+
+# Build et redémarrage des conteneurs
+docker-compose down
+docker-compose build --no-cache
+docker-compose up -d
+
+echo "Déploiement terminé."
